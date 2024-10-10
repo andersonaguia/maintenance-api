32574505d8a2d8f2446ed1d1f1d9e053aa7799e6
andersonaguia <andersonlaguiar@gmail.com>
2024-10-10 14:11:35 -0300

    instala ferramentas para validações

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { useContainer } from 'class-validator';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(3000);
+  Logger.log('Server is running on port 3000');
 }
 bootstrap();
